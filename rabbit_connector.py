a6d16b2cd33a4f480ecb7673bbfe598f2cf401cc
ga53fib <8809217+ozgurakyazi@users.noreply.github.com>
2019-11-30 22:29:48 +0100

    send_data and start_listening methods are now a part of connector, so the connecting parties are not bothered with it.

--- rabbit_connector.py
+++ rabbit_connector.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 class RabbitConnector():
 	'''
@@ -12,7 +12,6 @@ class RabbitConnector():
 		self.server_ip = server_ip   # server of the broker
 		self.queue_name = queue_name         # name of the queue/broker
 
-	def setup_connection(self):
 		'''
 		Sets up the connection and the queue with the name queue_name is created, so that
 		we make sure the queue exists.
@@ -21,3 +20,19 @@ class RabbitConnector():
 		self.channel = self.connection.channel()
 
 		self.channel.queue_declare(queue=self.queue_name)
+		
+
+	def send_data(self, data):
+		self.channel.basic_publish(
+			exchange="",
+			routing_key=self.queue_name,
+			body = json.dumps(data)
+		)
+
+	def start_listening(self,method):
+		self.channel.basic_consume(
+			queue=self.queue_name, on_message_callback=method, auto_ack=True)
+
+		### After this line, the sequential execution of the thread is blocked.!!
+		### CAREFUL###
+		self.channel.start_consuming()
