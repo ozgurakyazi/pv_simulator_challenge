8acd37ac4e6d23ac4b273bdfff5e9cf715944073
ga53fib <8809217+ozgurakyazi@users.noreply.github.com>
2019-11-30 11:02:25 +0100

    shared_params is used for such parameters

---
+++ shared_params.py
@@ -0,0 +1,13 @@
+'''
+Defines the shared parameters of the system.
+'''
+
+###  Connection Parameters
+server_ip = "localhost"   # server of the broker
+queue_name = "pv"         # name of the queue/broker
+
+
+
+###  Data Send/Receive Parameters
+datetime_format = "%m/%d/%Y, %H:%M:%S"
+
